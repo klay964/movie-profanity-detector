4e37058ad446eb3099cfb3b56196b21cf7350995
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2023-05-16 07:31:55 +0300

    add speech to text api

--- server/controllers/videoController.js
+++ server/controllers/videoController.js
@@ -1,25 +1,46 @@
 const catchAsync = require('../utils/catchAsync');
 const { exec } = require('child_process');
-const ffmpeg = require('fluent-ffmpeg');
+const sdk = require('microsoft-cognitiveservices-speech-sdk');
+const fs = require('fs');
 
 exports.getVideo = catchAsync(async (req, res, next) => {
-  const inputFilePath = '../videos/curses.mp4';
-  const outputDirectory = '../audios/';
-  const outputFileName = 'audio';
+  const command = `ffmpeg -i curses.mp4 -vn -c:a libmp3lame audio.wav`;
 
-  ffmpeg(inputFilePath)
-    .noVideo()
-    .audioCodec('libmp3lame')
-    .save(`${outputDirectory}${outputFileName}.mp3`)
-    .on('end', () => {
-      console.log(`Audio saved to ${outputDirectory}${outputFileName}.mp3`);
-    })
-    .on('error', (err) => {
-      console.error('Error:', err.message);
-    });
-  // Response
-  res.status(200).json({
-    status: 'success',
-    data: 'hi',
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      console.error(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`Audio saved`);
   });
+  console.log('dlskdsld');
+  const apiKey = '3a015525cf8145969e4c10bf06acfab6';
+  const region = 'eastus';
+  const speechConfig = sdk.SpeechConfig.fromSubscription(apiKey, region);
+
+  const audioConfig = sdk.AudioConfig.fromStreamInput('audio.wav');
+  speechConfig.enableDictation();
+  speechConfig.speechRecognitionLanguage = 'en-US';
+  const recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+  processmsg = '.';
+  recognizer.recognizing = (s, e) => {
+    processmsg = processmsg + '.';
+    console.log('processing .' + processmsg);
+  };
+  const outputmsg = '';
+  recognizer.recognized = (s, e) => {
+    outputmsg = outputmsg + e.result.text;
+    console.log('Output : ', outputmsg);
+    if (e.result.reason == ResultReason.RecognizedSpeech) {
+      console.log(`RECOGNIZED: Text=${e.result.text}`);
+    } else if (e.result.reason == ResultReason.NoMatch) {
+      console.log('NOMATCH: Speech could not be recognized.');
+    }
+  };
+  // Response
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: 'hi',
+  // });
 });
