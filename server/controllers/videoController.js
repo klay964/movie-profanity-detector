344f862afb327f2b0c6c38ba52096c2ab7a64f31
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2023-05-16 03:32:02 +0300

    add ffmpeg package

--- server/controllers/videoController.js
+++ server/controllers/videoController.js
@@ -1,6 +1,22 @@
 const catchAsync = require('../utils/catchAsync');
+const { exec } = require('child_process');
+const ffmpeg = require('fluent-ffmpeg');
 
 exports.getVideo = catchAsync(async (req, res, next) => {
+  const inputFilePath = '../videos/curses.mp4';
+  const outputDirectory = '../audios/';
+  const outputFileName = 'audio';
+
+  ffmpeg(inputFilePath)
+    .noVideo()
+    .audioCodec('libmp3lame')
+    .save(`${outputDirectory}${outputFileName}.mp3`)
+    .on('end', () => {
+      console.log(`Audio saved to ${outputDirectory}${outputFileName}.mp3`);
+    })
+    .on('error', (err) => {
+      console.error('Error:', err.message);
+    });
   // Response
   res.status(200).json({
     status: 'success',
